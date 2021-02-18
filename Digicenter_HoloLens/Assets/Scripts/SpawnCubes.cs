c1fec7c974b04c9b61c3344c8f244e538b20342b
lebbis <44391814+lebbis@users.noreply.github.com>
2021-02-18 14:18:22 +0200

    initial commit

---
+++ Digicenter_HoloLens/Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    public Vector3 distance = new Vector3(0f, 0.01f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    public void SpawnObject()
+    {
+        int randomIndex = Random.Range(0, objects.Length);
+        GameObject instantiateObject = Instantiate(objects[randomIndex], transform.position + (distance), transform.rotation);
+    }
+}
---
+++ Digicenter_HoloLens/Assets/Scripts/triggers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggers : MonoBehaviour
+{
+    public GameObject txt;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pallo"))
+        {
+            txt.SetActive(true);
+            Debug.Log("Jeejee triggeri toimii");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        txt.SetActive(false);
+    }
+}
